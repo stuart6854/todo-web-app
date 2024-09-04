206853533b7a3c1adee95745aa43c76fd3c8d926
Stuart Millman <stu.millman15@gmail.com>
2024-09-04 11:48:51 +0100

    Fix RegisterModel validation.

--- Domain/Models/RegisterModel.cs
+++ Domain/Models/RegisterModel.cs
@@ -6,10 +6,10 @@ public class RegisterModel
 {
     [Required]
     [Length(2, 18)]
-    [RegularExpression(@"^[\w.\-]$")]
+    [RegularExpression(@"^[\w.\-]+$")]
     public string Username { get; set; }
     [Required]
     [Length(8, 32)]
-    [RegularExpression(@"^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,32}$", ErrorMessage = "Password does not meet security rules.")]
+    [RegularExpression(@"^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s])+$", ErrorMessage = "Password does not meet security rules.")]
     public string Password { get; set; }
 }
